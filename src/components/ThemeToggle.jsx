b8e9e8c6ae68dd98e1b7985ad5989312cc03ebc5
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-12-18 03:29:14 +0700

    ThemeToggle cleancode

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -33,9 +33,8 @@ export const ThemeToggle = () => {
             onClick={toggleTheme}
             className={cn(
                 "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
-                "focus:outlin-hidden"
-            )}
-        >
+                "focus:outline-hidden"
+            )}>
             {isDarkMode ? (
                 <Sun className="h-6 w-6 text-yellow-300" />
             ) : (
