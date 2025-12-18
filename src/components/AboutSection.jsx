48da6354ddb4aa76b1f60b38deaada843b0067f1
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-12-18 17:21:58 +0700

    AboutSec update & cleancode

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,7 +3,6 @@ import { Briefcase, Code, User } from "lucide-react";
 export const AboutSection = () => {
     return (
         <section id="about" className="py-24 px-4 relative">
-            {" "}
             <div className="container mx-auto max-w-5xl">
                 <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
                     About <span className="text-primary"> Me</span>
@@ -30,14 +29,12 @@ export const AboutSection = () => {
 
                         <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
                             <a href="#contact" className="cosmic-button">
-                                {" "}
                                 Get In Touch
                             </a>
 
                             <a
                                 href=""
-                                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
-                            >
+                                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                                 Download CV
                             </a>
                         </div>
