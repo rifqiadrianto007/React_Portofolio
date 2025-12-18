131b08d778bfccadfff48fff00794c864fd87177
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-12-18 17:13:09 +0700

    Navbar update & cleancode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -32,10 +32,9 @@ export const Navbar = () => {
             <div className="container flex items-center justify-between">
                 <a
                     className="text-xl font-bold text-primary flex items-center"
-                    href="#hero"
-                >
+                    href="#hero">
                     <span className="relative z-10">
-                        <span className="text-glow text-foreground"> PedroTech </span>{" "}
+                        <span className="text-glow text-foreground"> Rifqi </span>
                         Portfolio
                     </span>
                 </a>
@@ -46,8 +45,7 @@ export const Navbar = () => {
                         <a
                             key={key}
                             href={item.href}
-                            className="text-foreground/80 hover:text-primary transition-colors duration-300"
-                        >
+                            className="text-foreground/80 hover:text-primary transition-colors duration-300">
                             {item.name}
                         </a>
                     ))}
@@ -58,28 +56,24 @@ export const Navbar = () => {
                 <button
                     onClick={() => setIsMenuOpen((prev) => !prev)}
                     className="md:hidden p-2 text-foreground z-50"
-                    aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
-                >
-                    {isMenuOpen ? <X size={24} /> : <Menu size={24} />}{" "}
+                    aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}>
+                    {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
                 </button>
 
                 <div
                     className={cn(
                         "fixed inset-0 bg-background/95 backdroup-blur-md z-40 flex flex-col items-center justify-center",
                         "transition-all duration-300 md:hidden",
-                        isMenuOpen
-                            ? "opacity-100 pointer-events-auto"
+                        isMenuOpen ? "opacity-100 pointer-events-auto"
                             : "opacity-0 pointer-events-none"
-                    )}
-                >
+                    )}>
                     <div className="flex flex-col space-y-8 text-xl">
                         {navItems.map((item, key) => (
                             <a
                                 key={key}
                                 href={item.href}
                                 className="text-foreground/80 hover:text-primary transition-colors duration-300"
-                                onClick={() => setIsMenuOpen(false)}
-                            >
+                                onClick={() => setIsMenuOpen(false)}>
                                 {item.name}
                             </a>
                         ))}
